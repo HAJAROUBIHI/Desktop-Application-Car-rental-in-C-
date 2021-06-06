f4f8b003571b88945da37244c00efc8fc5d85f63
HAJAROUBIHI <85461658+HAJAROUBIHI@users.noreply.github.com>
2021-06-06 23:41:08 +0100

    Add files via upload

---
+++ PrototypesFonctions.h
@@ -0,0 +1,25 @@
+
+
+void EnTete();
+void MenuPrincipal();
+
+void visualiserContrat(char *,float);
+int verifierClient(char *,int);
+void voitureDisponile(char *);
+void enLocation(char *,int,char *);
+void louerVoiture(char *);
+void retournerVoiture(char *,float);
+void modifierContrat(char *,float);
+void MenuLocation();
+
+void listeDesVoitures(char*);
+void ajouterVoiture(char*);
+void modifierVoiture(char *n,int);
+void supprimerVoiture(char *,int);
+void MenuGestionVoiture();
+
+void listeDesClients(char*);
+void ajouterClient(char*);
+void modifierClient(char *,int);
+void supprimerClient(char *,int);
+void MenuGestionClient();
---
+++ StructuresProjet.h
@@ -0,0 +1,45 @@
+
+
+typedef struct Date{
+  int annee;
+  int mois;
+  int jour;
+}date;
+
+
+typedef struct Voiture
+{
+ int idVoiture;
+ char marque[15];
+ char nomVoiture[15];
+ char couleur[7];
+ int nbplaces;
+ int prixJour;
+ char EnLocation[4];
+} voiture;
+
+
+typedef struct ContratLocation
+{
+ float numContrat;
+ int idVoiture;
+ int idClient;
+ date debut;
+ date fin;
+ int Cout;
+}contrat;
+
+
+
+typedef struct Client
+{
+ int idClient;
+ char nom[20];
+ char prenom[20];
+ int Cin;
+ char adresse[15];
+ int telephone;
+}client;
+
+
+
